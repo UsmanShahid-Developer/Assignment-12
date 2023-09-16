cafdecaa545ab5a7356294c52d59dccae7e5da5f
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-09-16 11:35:05 +0500

    new commit

--- app/page.js
+++ app/page.js
@@ -56,18 +56,18 @@ export default function Home() {
     <>
 
 
-      <div className="bg-[#D6EFFF] pt-10 pb-72 min-w-full ">
+      <div className="    bg-green-100	py-5	 pt-10 pb-72 min-w-full ">
         <div className="    ">
           <div className="text-[#000] text-7xl text-center">Use Github API</div>
 
 
           {data2.map((userData, i) => (
-            <div key={i} className="text-3xl	  border-[#000]">
+            <div key={i} className="text-base		  border-[#000]">
               <div className="grid justify-items-center">
                 <div className="px-6 py-3   text-center font-medium text-[#000]  rounded-full"> <img src={userData.avatar_url} alt="asd" width={250} className="rounded-full text-center" height={250} /></div>
               </div>
               <div className=" flex justify-around">
-                <div className="px-6 py-3 text-left text-l font-medium text-[#000] uppercase tracking-wider"> Name : {userData.name}</div>
+                <div className="px-6 py-3 text-left text-1 font-small text-[#000] uppercase tracking-wider"> Name : {userData.name}</div>
                 <div className="px-6 py-3 text-left text-l font-medium text-[#000] uppercase tracking-wider"> Followers :{userData.followers}</div>
                 <div className="px-6 py-3 text-left text-l font-medium text-[#000] uppercase tracking-wider"> Following :{userData.following}</div>
                 <div className="px-6 py-3 text-left text-l font-medium text-[#000] uppercase tracking-wider"> Public repos : {userData.public_repos}</div>
@@ -83,16 +83,16 @@ export default function Home() {
 
           <div className="text-center py-10">
             <input
-              className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+              className="border border-red-400 rounded-lg px-20 py-5 "
               type="text"
               placeholder="Enter your user name"
               value={userName}
               onChange={handleChange}
             />
-          </div>
-          <div className="text-center" onClick={handleGet2}><button onClick={handleGet} className="bg-[#991b1b] hover:bg-[#991b1b] text-white font-bold py-2 px-4 rounded">
+          <span className="text-center" onClick={handleGet2}><button onClick={handleGet} className="bg-[#991b1b] hover:bg-[#991b1b] text-white font-bold py-5 px-6 rounded-lg ms-5">
             Get Data
           </button>
+          </span>
           </div>
 
 
@@ -100,13 +100,12 @@ export default function Home() {
             <table className="min-w-full">
               <thead>
                 <tr className="border-b  font-medium  border-[#000]">
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">#</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">Profile</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">Name</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">Followers</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">Following</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">Public repos</th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">Github</th>
+                  <th className="px-6 py-3 text-left text-base	 font-medium text-[#000] uppercase tracking-wider">#</th>
+                  <th className="px-6 py-3 text-left text-3xl font-medium text-[#000] uppercase tracking-wider">Profile</th>
+                  <th className="px-6 py-3 text-left text-3xl font-medium text-[#000] uppercase tracking-wider">Name</th>
+                  <th className="px-6 py-3 text-left text-3xl font-medium text-[#000] uppercase tracking-wider">Followers</th>
+                  <th className="px-6 py-3 text-left text-3xl font-medium text-[#000] uppercase tracking-wider">Following</th>
+                  
 
                 </tr>
               </thead>
@@ -115,13 +114,12 @@ export default function Home() {
               <tbody>
                 {data.map((userData, i) => (
                   <tr key={i} className="border-b  font-medium  border-[#000]">
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">{i + 1}</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider rounded-full"> <img src={userData.avatar_url} alt="asd" width={75} className="rounded-full" height={75} /></th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">{userData.login}</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">{userData.followers}</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">{userData.following}</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">{userData.public_repos}</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-[#000] uppercase tracking-wider">{userData.url}</th>
+                    <th className="px-6 py-3 text-left text-1xl font-medium text-[#000] uppercase tracking-wider">{i + 1}</th>
+                    <th className="px-6 py-3 text-left text-1xl font-medium text-[#000] uppercase tracking-wider rounded-full"> <img src={userData.avatar_url} alt="asd" width={75} className="rounded-full" height={75} /></th>
+                    <th className="px-6 py-3 text-left text-1xl font-medium text-[#000] uppercase tracking-wider">{userData.login}</th>
+                    <th className="px-6 py-3 text-left text-1xl font-medium text-[#000] uppercase tracking-wider">{userData.followers}</th>
+                    <th className="px-6 py-3 text-left text-1xl font-medium text-[#000] uppercase tracking-wider">{userData.following}</th>
+            
 
                   </tr>
                 ))}
